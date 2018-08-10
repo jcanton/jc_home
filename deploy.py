a20b36c75c8868b65b4d358e95647f9261746b44
Jacopo Canton <jcanton@mech.kth.se>
2018-08-10 11:32:40 +0200

    big stuff

--- deploy.py
+++ deploy.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
-import os, shutil, socket
+import os, shutil, socket, platform
 #
 #==============================================================================
-# get hostname
+# get hostname and homedir
 #==============================================================================
 
 hostname = socket.gethostname()
+homedir  = os.path.expanduser('~')
+cdir     = os.getcwd()
+system   = platform.system()
 
 #==============================================================================
 # deploy configuration files to home folder
@@ -13,129 +16,100 @@ hostname = socket.gethostname()
 #
 print('\n')
 print('--- Deploying configuration files to home directory ---')
-print('    hostname: ' + hostname)
+print('    hostname:       ' + hostname)
+print('    system:         ' + system)
+print('    home directory: ' + homedir)
 print('\n')
 
 #==============================================================================
-# clear any previously existig folders
+# Function definition
 #==============================================================================
 #
-usrInVim = 'n'
-if os.path.exists('../.vim'):
-	usrInVim = raw_input('    Found .vim folder. Delete? y/[n] ')
-	if (usrInVim == 'y'):
-		print('    Removing old .vim folder')
-		shutil.rmtree('../.vim')
-else:
-	usrInVim = 'y'
+def delete(dpath):
+	if os.path.isdir(dpath):
+		shutil.rmtree(dpath)
+	else:
+		os.remove(dpath)
+
+def check_delete_link(fname, lfname=''):
+	if (lfname == ''):
+		lfname = fname
+	fpath  = os.path.join(homedir, fname)
+	lfpath = os.path.join(cdir,    lfname)
+	if os.path.exists(fpath):
+		if (not os.path.islink(fpath)):
+			# old file
+			usrIn = raw_input('\tFound '+fname+' delete? y/[n] ')
+			if (usrIn == 'y'):
+				print('    Removing old '+fname)
+				delete(fpath)
+				os.symlink(lfpath, fpath)
+		else:
+			# symlink
+			if (not os.readlink(fpath) == lfpath):
+				delete(fpath)
+				os.symlink(lfpath, fpath)
+	else:
+		# did not exist
+		os.symlink(lfpath, fpath)
+
+#==============================================================================
+# Deploy
+#==============================================================================
 
 #------------------------------------------------------------------------------
 # deploy bashrc
 #
-shutil.copy('.bashrc', '../')
+check_delete_link('.bashrc')
 
-if (hostname == 'arch10'):
-	shutil.copy('.bashrc.local.arch10',    '../.bashrc.local')
-elif (hostname == 'arch14'):
-	shutil.copy('.bashrc.local.arch14',    '../.bashrc.local')
-elif (hostname == 'erebos'):
-	shutil.copy('.bashrc.local.erebos',    '../.bashrc.local')
-elif (hostname == 'triolith1'):
-	shutil.copy('.bashrc.local.arch14',    '../.bashrc.local')
-elif (hostname == 'bernoulli'):
-	shutil.copy('.bashrc.local.bernoulli', '../.bashrc.local')
-elif (hostname == 'mac13'):
-	shutil.copy('.bashrc.local.mac13', '../.bashrc.local')
-	shutil.copy('.bash_profile.mac13', '../.bash_profile')
-elif (hostname == 'work13' ):
-	shutil.copy('.bashrc.local.work13', '../.bashrc.local')
-	shutil.copy('.bash_profile.mac13',  '../.bash_profile')
+check_delete_link('.bashrc.local', '.bashrc.local.'+hostname)
+
+if (system == 'Darwin'):
+	check_delete_link('.bash_profile', '.bash_profile.osx')
 
 #------------------------------------------------------------------------------
 # deploy git
 #
-shutil.copy('.gitconfig', '../')
-shutil.copy('.git-completion.bash', '../')
+check_delete_link('.gitconfig')
+check_delete_link('.git-completion.bash')
 
 #------------------------------------------------------------------------------
 # deploy ipython config
 #
-if not os.path.exists('../.ipython/profile_default'):
-	print('   CREATING .ipython/profile_default folder')
-	os.makedirs('../.ipython/profile_default')
-
-shutil.copy('.ipython/profile_default/ipython_config.py', '../.ipython/profile_default/')
+check_delete_link('.ipython')
 
 #------------------------------------------------------------------------------
 # deploy matplotlib
 #
-if (hostname == 'mac13' or hostname == 'work13' ):
-	if not os.path.exists('../.matplotlib'):
-		print('   CREATING .matplotlib folder')
-		os.makedirs('../.matplotlib')
-	shutil.copy('.config/matplotlib/matplotlibrc', '../.matplotlib/')
-else:
-	if not os.path.exists('../.config/matplotlib'):
-		print('   CREATING .config/matplotlib folder')
-		os.makedirs('../.config/matplotlib')
-	shutil.copy('.config/matplotlib/matplotlibrc', '../.config/matplotlib/')
+if (system == 'Darwin'):
+	check_delete_link('.matplotlib', 'matplotlib')
+elif (system == 'Linux'):
+	check_delete_link('.config/matplotlib', 'matplotlib')
 
 #------------------------------------------------------------------------------
 # deploy redshift
 #
-if (hostname != 'mac13' and hostname != 'work13'):
-	if (hostname == 'arch10'):
-		shutil.copy('.config/redshift.conf.arch10', '../.config/redshift.conf')
-	else:
-		shutil.copy('.config/redshift.conf.erebos', '../.config/redshift.conf')
+if (system == 'Linux'):
+	check_delete_link('.config/redshift.conf', 'redshift.conf.'+hostname)
 
 #------------------------------------------------------------------------------
 # deploy terminator
 #
-if (hostname != 'mac13' and hostname != 'work13'):
-	if not os.path.exists('../.config/terminator'):
-		print('   CREATING .config/terminator folder')
-		os.makedirs('../.config/terminator')
-	#
-	if (hostname == 'arch10'):
-		shutil.copy('.config/terminator/config.arch10',    '../.config/terminator/config')
-	elif (hostname == 'arch14'):
-		shutil.copy('.config/terminator/config.arch14',    '../.config/terminator/config')
-	elif (hostname == 'erebos'):
-		shutil.copy('.config/terminator/config.erebos',    '../.config/terminator/config')
-	elif (hostname == 'bernoulli'):
-		shutil.copy('.config/terminator/config.bernoulli', '../.config/terminator/config')
+if (system == 'Linux'):
+	check_delete_link('.config/terminator', 'terminator')
 
 #------------------------------------------------------------------------------
 # deploy vim
 #
-if (usrInVim == 'y'):
-	shutil.copytree('.vim', '../.vim')
-
-shutil.copy('.vimrc',       '../')
-
-if (hostname == 'arch10'):
-	shutil.copy('.vimrc.local.arch10',    '../.vimrc.local')
-elif (hostname == 'arch14'):
-	shutil.copy('.vimrc.local.arch14',    '../.vimrc.local')
-elif (hostname == 'erebos'):
-	shutil.copy('.vimrc.local.erebos',    '../.vimrc.local')
-elif (hostname == 'bernoulli'):
-	shutil.copy('.vimrc.local.bernoulli', '../.vimrc.local')
-elif (hostname == 'triolith1'):
-	shutil.copy('.vimrc.local.erebos',    '../.vimrc.local')
-elif (hostname == 'mac13' or hostname == 'work13'):
-	shutil.copy('.vimrc.local.mac13', '../.vimrc.local')
-	shutil.copy('mvim', '../.vim/')
+check_delete_link('.vim')
+check_delete_link('.vimrc')
+check_delete_link('.vimrc.local')
 
 #------------------------------------------------------------------------------
 # deploy xoural
 #
-if (hostname != 'mac13' and hostname != 'work13'):
-	if not os.path.exists('../.xournal'):
-		os.makedirs('../.xournal')
-	
-	shutil.copy('.xournal/config', '../.xournal/')
+if (system == 'Linux'):
+	check_delete_link('.xournal')
 
 #==============================================================================
 #==============================================================================
