81928935489a1d2b23822c51811897bbbc893e54
Jacopo Canton <jacopo.canton@gmail.com>
2018-09-26 15:45:02 +0200

    auto add git submodules in deploy

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os, shutil, socket, platform
+from subprocess import call
 #
 #==============================================================================
 # get hostname and homedir
@@ -32,6 +33,8 @@ def delete(dpath):
 		os.remove(dpath)
 
 def check_delete_link(fname, lfname=''):
+	#  fname: file name that will be in the system (symlink)
+	# lfname: name of the same file here in the repo
 	if (lfname == ''):
 		lfname = fname
 	fpath  = os.path.join(homedir, fname)
@@ -52,6 +55,12 @@ def check_delete_link(fname, lfname=''):
 	else:
 		# did not exist
 		os.symlink(lfpath, fpath)
+
+#==============================================================================
+# Initialise git submodules
+#==============================================================================
+call("git submodule init",   shell=True)
+call("git submodule update", shell=True)
 
 #==============================================================================
 # Deploy
@@ -105,16 +114,21 @@ elif (system == 'Linux'):
 #	check_delete_link('.config/terminator', 'terminator')
 
 #------------------------------------------------------------------------------
+# deploy tmux
+#
+check_delete_link('.tmux',             'tmux-config/tmux')
+check_delete_link('.tmux.conf',        'tmux-config/tmux/tmux.conf')
+check_delete_link('.tmux.remote.conf', 'tmux-config/tmux/tmux.remote.conf')
+os.chdir('tmux-config')
+call('./install.sh', shell=True)
+os.chdir('..')
+
+#------------------------------------------------------------------------------
 # deploy vim
 #
 check_delete_link('.vim')
 check_delete_link('.vimrc')
 check_delete_link('.vimrc.local')
-
-#------------------------------------------------------------------------------
-# deploy tmux
-#
-check_delete_link('.tmux.conf')
 
 ##------------------------------------------------------------------------------
 ## deploy xoural (LET'S FORGET ABOUT THIS)
