a710248cd6b676947c21c02ec4be95df88845bed
Jacopo Canton <jacopo.canton@gmail.com>
2019-04-02 14:23:21 +0200

    more self compiled software

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,35 @@ cd src
 ./configure --prefix=$TARGET --with-features=huge
 make -j$JOBS
 make install
+
+# git
+cd $TARGET/repo
+git clone --depth 1 https://github.com/git/git.git || true
+cd git
+git pull
+make configure
+./configure --prefix=$TARGET
+make -j$JOBS
+make install
+
+
+# libevent (needed by tmux)
+cd $TARGET/repo
+git clone --depth 1 https://github.com/libevent/libevent.git || true
+cd libevent
+git pull
+./autogen.sh
+./configure --prefix=$TARGET
+make -j$JOBS
+make install
+
+
+# tmux
+cd $TARGET/repo
+git clone --depth 1 https://github.com/tmux/tmux.git || true
+cd tmux
+git pull
+./autogen.sh
+./configure --prefix=$TARGET
+make -j$JOBS
+make install
