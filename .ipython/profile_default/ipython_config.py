ea52d4d35c1f1b4070599876eda00c121c529dda
Jacopo <jacopo.canton@gmail.com>
2024-02-12 15:58:37 +0100

    fixed ipython autocomplete

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -8,6 +8,10 @@ c.InteractiveShellApp.exec_lines = [
     '%autoreload 2',
     'import numpy as np',
     'import matplotlib.pyplot as plt',
-    'np.set_printoptions(precision=4,suppress=False,linewidth=300)'
+    'np.set_printoptions(precision=4,suppress=False,linewidth=300)',
+    #'import IPython',
+    #'terminal = IPython.get_ipython()',
+    #'terminal.pt_app.auto_suggest = None'
 ]
 c.InteractiveShellApp.exec_files = ['functions.py'] 
+c.TerminalInteractiveShell.autosuggestions_provider = None
