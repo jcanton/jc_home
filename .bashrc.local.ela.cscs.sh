7049f3b1cc1ab533bc0b4de7d36763f38066563e
Jacopo <jacopo.canton@gmail.com>
2024-05-27 14:19:51 +0200

    updates

--- .bashrc.local.ela.cscs.sh
+++ .bashrc.local.ela.cscs.sh
@@ -11,6 +11,51 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+if [ -n "${VSCODE_INVOKING}" ]; then
+    #echo "invoked by vscode"
+    # set colors
+    export CLICOLOR=1
+    # Base16 Shell
+    /bin/sh $HOME/jc_home/base16-shell/scripts/base16-solarized-light.sh
+fi
+export GT4PY_BUILD_CACHE_LIFETIME=PERSISTENT
+
+export OMP_NUM_THREADS=12
+alias sq='squeue -u jcanton -o "%.8i %.8u %.7a %.9P %.30j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E"'
+alias sql='squeue -u jcanton -o "%.8i %.8u %.7a %.9P %.50j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E"'
+function wsq {
+    watch -n 1 "squeue -u jcanton -o '%.8i %.8u %.7a %.9P %.30j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E'"
+}
+
+case $HOSTNAME in
+    daint*)
+        rm $HOME/.vscode-server
+        ln -s $HOME/.vscode-server.daint $HOME/.vscode-server
+        export GT4PY_BUILD_CACHE_DIR=/scratch/snx3000/jcanton/icon4py_tmp
+        #
+        rm $HOME/miniconda3
+        ln -s /scratch/jcanton/miniconda3 $HOME/miniconda3
+        ;;
+    tsa*)
+        rm $HOME/.vscode-server
+        ln -s $HOME/.vscode-server.tsa $HOME/.vscode-server
+        export GT4PY_BUILD_CACHE_DIR=/scratch/jcanton/icon4py_tmp
+        #
+        rm $HOME/miniconda3
+        ln -s /scratch/jcanton/miniconda3 $HOME/miniconda3
+        ;;
+    nid*)
+        alias pyNode='source ~/.pythonComputeNode.env'
+        ;;
+    ela?)
+        echo "We're on ela"
+        # Though ela does not have access to /project anymore...
+        ;;
+    *)
+        echo "hostname not recognized in .bashrc.local"
+        ;;
+esac
+
 # # PALM
 # export LD_LIBRARY_PATH="/store/g142/jcanton/repos/palm_build/rrtmg/lib:${LD_LIBRARY_PATH}"
 # export PATH="/store/g142/jcanton/repos/palm_build/bin:${PATH}"
@@ -27,47 +72,3 @@ export NVM_DIR="$HOME/.nvm"
 # export PATH="/project/s83c/fieldextra/daint/tools:$PATH"
 # alias fieldextra='/project/s83c/fieldextra/daint/bin/fieldextra_gnu_opt_omp'
 # alias grins='/project/s83c/fieldextra/daint/tools/grins'
-
-if [ -n "${VSCODE_INVOKING}" ]; then
-    #echo "invoked by vscode"
-    # set colors
-    export CLICOLOR=1
-    # Base16 Shell
-    /bin/sh $HOME/jc_home/base16-shell/scripts/base16-solarized-light.sh
-fi
-export GT4PY_BUILD_CACHE_LIFETIME=PERSISTENT
-
-case $HOSTNAME in
-    daint*)
-        export OMP_NUM_THREADS=12
-        alias sq='squeue -u jcanton -o "%.8i %.8u %.7a %.9P %.30j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E"'
-        alias sql='squeue -u jcanton -o "%.8i %.8u %.7a %.9P %.50j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E"'
-        function wsq {
-            watch -n 1 "squeue -u jcanton -o '%.8i %.8u %.7a %.9P %.30j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E'"
-        }
-        # rm $HOME/.vscode-server
-        # ln -s $HOME/.vscode-server.daint $HOME/.vscode-server
-        export GT4PY_BUILD_CACHE_DIR=/scratch/snx3000/jcanton/icon4py_tmp
-        ;;
-    tsa*)
-        export OMP_NUM_THREADS=12
-        alias sq='squeue -u jcanton -o "%.8i %.8u %.7a %.9P %.30j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E"'
-        # alias sql='squeue -u jcanton -o "%.8i %.8u %.7a %.9P %.50j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E"'
-        # function wsq {
-        #     watch -n 1 "squeue -u jcanton -o '%.8i %.8u %.7a %.9P %.30j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E'"
-        # }
-        rm $HOME/.vscode-server
-        ln -s $HOME/.vscode-server.tsa $HOME/.vscode-server
-        export GT4PY_BUILD_CACHE_DIR=/scratch/jcanton/icon4py_tmp
-        ;;
-    nid*)
-        alias pyNode='source ~/.pythonComputeNode.env'
-        ;;
-    ela?)
-        echo "We're on ela"
-        # Though ela does not have access to /project anymore...
-        ;;
-    *)
-        echo "hostname not recognized in .bashrc.local"
-        ;;
-esac
