c9c27645d80fd30044cdde9fe38cb1d857a641b2
Jacopo <jacopo.canton@gmail.com>
2024-12-18 10:34:45 +0000

    better mounting

--- .bashrc.sshMount.sh
+++ .bashrc.sshMount.sh
@@ -1,52 +1,65 @@
 sshmount() {
 
     diskName=$1
-    diskDir=$HOME/sshfsVolumes/$diskName
+    diskDir="${HOME}/sshfsVolumes/${diskName}"
+
+    unameOut="$(uname -s)"
+    if [ $(uname -s) == "Darwin" ]; then
+        umount_cmd="diskutil umount force"
+        dst_options="-o auto_cache,reconnect,defer_permissions,noappledouble"
+        tunnel_options="-o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa"
+    elif [ $(uname -s) == "Linux" ]; then
+        umount_cmd="fusermount3 -u"
+        dst_options=""
+        tunnel_options="-o IdentityFile=${HOME}/.ssh/id_rsa"
+    else
+        echo "unknown system"
+    fi
 
     cd "$HOME" || exit
     if [[ ! -e ${diskDir} ]]; then
         mkdir -p "$diskDir"
     else
-        diskutil umount force "$diskDir"
+        eval "${umount_cmd} ${diskDir}"
     fi
 
     if [ "$diskName" == "ela" ]; then
-        sshfs ela:/users/jcanton/  "$diskDir"  -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs ela:/users/jcanton/ ${diskDir} ${dst_options}"
 
     elif [ "$diskName" == "daint" ]; then
-        sshfs daint:/users/jcanton/  "$diskDir"  -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs daint:/users/jcanton/ ${diskDir} ${dst_options}"
 
     elif [ "$diskName" == "daintScratch" ]; then
-        sshfs daint:/scratch/snx3000/jcanton/  "$diskDir"  -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs daint:/scratch/snx3000/jcanton/ ${diskDir} ${dst_options}"
 
     elif [ "$diskName" == "pr133" ]; then
-        sshfs daint:/project/pr133/jcanton/  "$diskDir"  -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs daint:/project/pr133/jcanton/ ${diskDir} ${dst_options}"
 
     elif [ "$diskName" == "g142" ]; then
-        sshfs daint:/store/g142/jcanton/  "$diskDir"  -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs daint:/store/g142/jcanton/ ${diskDir} ${dst_options}"
 
     elif [ "$diskName" == "d121" ]; then
-        sshfs daint:/project/d121/jcanton/  "$diskDir"  -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs daint:/project/d121/jcanton/ ${diskDir} ${dst_options}"
 
     elif [ "$diskName" == "tsa" ]; then
-        sshfs tsa:/scratch/jcanton/  "$diskDir"  -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs tsa:/scratch/jcanton/ ${diskDir} ${dst_options}"
 
     elif [ "$diskName" == "balfrin" ]; then
-        sshfs balfrin:/scratch/mch/jcanton/  "$diskDir"  -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs balfrin:/scratch/mch/jcanton/ ${diskDir} ${dst_options}"
 
     elif [ "$diskName" == "co2" ]; then
-        sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa jcanton@fog2.ethz.ch:/net/co2/exclaim/jcanton  "$diskDir"  -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs ${tunnel_options} jcanton@fog2.ethz.ch:/net/co2/exclaim/jcanton ${diskDir} ${dst_options}"
 
     elif [ "$diskName" == "pi" ]; then
-        sshfs pi:/home/pi  "$diskDir"  -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs pi:/home/pi ${diskDir} ${dst_options}"
 
     elif [ "$diskName" == "ciccia" ]; then
-        sshfs ciccia:/var/services/homes/jacopo "$diskDir" -o auto_cache,reconnect,defer_permissions,noappledouble
+        command="sshfs ciccia:/var/services/homes/jacopo ${diskDir} ${dst_options}"
 
     else
         echo "Unknown host name"
-
     fi
 
+    eval $command
     cd "$diskDir" || exit
 }
