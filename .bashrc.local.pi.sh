a258daa054e2d296fce81c40bc083172561b94bc
Jacopo <jacopo.canton@gmail.com>
2025-01-20 11:32:31 +0100

    brightness control

--- .bashrc.local.pi.sh
+++ .bashrc.local.pi.sh
@@ -25,3 +25,10 @@ base16_solarized-light
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+set_brightness() {
+    brightness_value=$1
+
+    command="ddcutil --display 1 setvcp 10 ${brightness_value}"
+    eval $command
+}
