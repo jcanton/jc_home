ebf92d2d903edf3289c27f769989d009f7acbb65
Jacopo <jacopo.canton@gmail.com>
2025-01-28 08:19:55 +0100

    updates for pi

--- i3/startup_programs.sh
+++ i3/startup_programs.sh
@@ -5,10 +5,10 @@ i3-msg 'workspace 1:loc; exec chromium'
 sleep 5
 #i3-msg 'workspace 1:loc; exec chromium --profile-directory=Default --app-id=pjibgclleladliembfgfagdaldikeohf'
 i3-msg 'workspace 1:loc; exec i3-sensible-terminal'
-sleep 5
+#sleep 5
 
-# 2
-i3-msg 'workspace 2:code; exec i3-sensible-terminal'
-sleep 5
-i3-msg 'workspace 2:code; exec code'
-sleep 5
+# # 2
+# i3-msg 'workspace 2:code; exec i3-sensible-terminal'
+# sleep 5
+# i3-msg 'workspace 2:code; exec code'
+# sleep 5
