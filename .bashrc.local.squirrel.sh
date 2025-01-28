6819e5267c73ff4ebbd27ca7e5828273aa933601
Jacopo <jacopo.canton@gmail.com>
2025-01-28 08:37:37 +0100

    add squirrel

---
+++ .bashrc.local.squirrel.sh
@@ -0,0 +1,9 @@
+export HTTP_PROXY=proxy.ethz.ch:3128
+export HTTPS_PROXY=proxy.ethz.ch:3128
+export FTP_PROXY=proxy.ethz.ch:3128
+export NO_PROXY="localhost,127.0.0.1,::1"
+
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
